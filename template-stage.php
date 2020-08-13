f47adaf0e2ebf8cb978d220de006f70a5ebb65fa
Tom Hoadley <hoadleytom@gmail.com>
2020-08-13 10:45:35 +0100

    Pushin

--- template-stage.php
+++ template-stage.php
@@ -19,32 +19,49 @@ if ( have_posts() ) {
             $video_placeholder_string = 'style="background-image:url('.$video_placeholder.');"';
         }
         $custom_embed = get_field( 'custom_embed' );
+        $show_holding_content = get_field( 'show_holding_content' );
+        $holding_content_text = get_field( 'holding_content_text' );
     ?>
 
     <?php get_template_part('resources/parts/stage-header'); ?>
     <?php get_template_part('resources/parts/stage-banner'); ?>
 
-    <div class="content-container">
-        
-       <div class="content">
-            <?php if($video_id) { ?>
-                <div class="video-container" <?= $video_placeholder_string; ?>>
-                    <?php echo "<iframe src='https://player.vimeo.com/video/$video_id?title=0&byline=0&portrait=0&sidedock=0&autoplay=1'  frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>"; ?>
+    <?php if( $show_holding_content == 1 ) : ?>
+        <div class="content-container">
+            <div class="stage-banner-container content-container">
+                <div class="row">
+                    <div class="block">
+                        <p>
+                            <span>
+                                <?= $holding_content_text; ?>
+                            </span>      
+                        </p>
+                    </div>
                 </div>
-            <?php }; ?>
-            <?php if($custom_embed) { ?>
-                <div class="video-container">
-                    <?= $custom_embed; ?>
-                </div>
-            <?php } ?>
-            
-            <?php if ($caption) { ?>
-                <div class="caption">
-                    <?= $caption; ?>
-                </div>
-            <?php } ?>
-       </div>
-    </div>
+            </div>        
+        </div>       
+    <?php else : ?>
+        <div class="content-container">
+            <div class="content">
+                <?php if($video_id) { ?>
+                    <div class="video-container" <?= $video_placeholder_string; ?>>
+                        <?php echo "<iframe src='https://player.vimeo.com/video/$video_id?title=0&byline=0&portrait=0&sidedock=0&autoplay=1'  frameborder='0' webkitallowfullscreen mozallowfullscreen allowfullscreen ></iframe>"; ?>
+                    </div>
+                <?php }; ?>
+                <?php if($custom_embed) { ?>
+                    <div class="video-container">
+                        <?= $custom_embed; ?>
+                    </div>
+                <?php } ?>
+                
+                <?php if ($caption) { ?>
+                    <div class="caption">
+                        <?= $caption; ?>
+                    </div>
+                <?php } ?>
+            </div>
+        </div>
+    <?php endif; ?>    
     <?php get_template_part('resources/parts/decorations'); ?>
 <?php }; 
     }; 
